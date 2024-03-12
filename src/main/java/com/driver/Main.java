1e99fcb13dcb5ffec3bfc8d34057422bbef4a14c
Prathik Mishra <126965610+Prathik-mishra@users.noreply.github.com>
2024-03-12 15:46:00 +0530

    adding encapsulation code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+    RWOnly obj = new RWOnly();
+
+     obj.name = "";
+     System.out.println(obj.name);
+
+    //error message 'name' has private access in 'com.driver.RWOnly'
+
+    obj.setName("Prathik");
+    String name = obj.getName();
+  }
 }
