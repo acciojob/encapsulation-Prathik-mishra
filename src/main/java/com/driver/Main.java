f2a306d76ffbc280d2afe6eca387f152aa73e665
Prathik Mishra <126965610+Prathik-mishra@users.noreply.github.com>
2024-03-12 15:48:20 +0530

    fixed the error

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,8 +4,8 @@ public class Main {
   public static void main(String[] args) {
     RWOnly obj = new RWOnly();
 
-     obj.name = "";
-     System.out.println(obj.name);
+//     obj.name = "";
+//     System.out.println(obj.name);
 
     //error message 'name' has private access in 'com.driver.RWOnly'
 
